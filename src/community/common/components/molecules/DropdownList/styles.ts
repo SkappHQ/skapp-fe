5d2a98ce662c4eb70f8b0e396087dd1775432cd3
TuanFazidSamoon <fazid.samoon@rootcode.io>
2024-12-09 13:24:42 +0530

    font weights in people directory changed to 400

--- src/community/common/components/molecules/DropdownList/styles.ts
+++ src/community/common/components/molecules/DropdownList/styles.ts
@@ -11,7 +11,7 @@ export const styles = (theme: Theme) => ({
   },
 
   labelStyle: (isDisabled: boolean, error: boolean) => ({
-    fontWeight: 500,
+    fontWeight: 400,
     color: isDisabled
       ? theme.palette.text.disabled
       : error
