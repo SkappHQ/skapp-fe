9326eb7ca757730bf6a18c29fa9faf4b65d29822
erangi-ar <111747955+erangi-ar@users.noreply.github.com>
2024-12-11 16:45:51 +0530

    fix: hotjar integration improved

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,10 @@ import App, { AppContext } from "next/app";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import { hotjar } from "react-hotjar";
 import { I18nextProvider, useSSR } from "react-i18next";
 
 import BaseLayout from "~community/common/components/templates/BaseLayout/BaseLayout";
+import { appModes } from "~community/common/constants/configs";
 import { ZIndexEnums } from "~community/common/enums/CommonEnums";
 import { ToastProvider } from "~community/common/providers/ToastProvider";
 import { WebSocketProvider } from "~community/common/providers/WebSocketProvider";
@@ -18,6 +18,7 @@ import { theme } from "~community/common/theme/theme";
 import { themeSelector } from "~community/common/theme/themeSelector";
 import { MyAppPropsType } from "~community/common/types/CommonTypes";
 import { getDataFromLocalStorage } from "~community/common/utils/accessLocalStorage";
+import { initializeHotjar } from "~enterprise/common/utils/monitoring";
 import i18n from "~i18n";
 import "~styles/global.css";
 
@@ -33,12 +34,6 @@ function MyApp({
   const [queryClient] = useState(() => new QueryClient());
   useSSR(initialI18nStore, initialLanguage);
 
-  // added for testing purpose, should be removed once the MT setup is done
-  const getRandomTenant = () => {
-    const items = ["tenant1", "tenant2", "tenant3"];
-    return items[Math.floor(Math.random() * items?.length)];
-  };
-
   useEffect(() => {
     if (getDataFromLocalStorage("brandingData")) {
       const selectedTheme = themeSelector(
@@ -49,19 +44,8 @@ function MyApp({
       setNewTheme(theme);
     }
 
-    if (process.env.NODE_ENV === "production") {
-      hotjar.initialize({
-        id: Number(process.env.NEXT_PUBLIC_HOTJAR_SITE_ID),
-        sv: Number(process.env.NEXT_PUBLIC_HOTJAR_VERSION)
-      });
-
-      // Retrieve tenant ID from the random function temporarily
-      const tenantId = getRandomTenant() ?? "default-tenant";
-
-      // Record tenant information in Hotjar
-      if (hotjar.initialized()) {
-        hotjar.identify(`tenant`, { tenantId });
-      }
+    if (process.env.MODE === appModes.ENTERPRISE) {
+      initializeHotjar();
     }
   }, []);
 
