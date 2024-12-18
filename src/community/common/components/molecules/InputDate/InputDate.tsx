277e9c972cada017e1d50819ee7efaa81818daad
GimhaniRootCode <gimhani.samarasinghe@rootcodelabs.com>
2024-12-18 11:22:53 +0530

    fix: carry forward calendar

--- src/community/common/components/molecules/InputDate/InputDate.tsx
+++ src/community/common/components/molecules/InputDate/InputDate.tsx
@@ -87,7 +87,8 @@ const InputDate: FC<Props> = ({
   placeholder,
   popperStyles,
   selectedDate,
-  setSelectedDate
+  setSelectedDate,
+  inputFormat
 }) => {
   const theme: Theme = useTheme();
   const classes = styles();
@@ -240,7 +241,10 @@ const InputDate: FC<Props> = ({
           }}
         >
           {selectedDate
-            ? convertDateToFormat(selectedDate.toJSDate(), REVERSE_DATE_FORMAT)
+            ? convertDateToFormat(
+                selectedDate.toJSDate(),
+                inputFormat ? inputFormat : REVERSE_DATE_FORMAT
+              )
             : placeholder}
         </Typography>
         <Box onClick={(e) => !disabled && handleClick(e)}>
