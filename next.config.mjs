6238a38d1b652b182340e6150c54c7df7a80e8ea
Akmal Alavi <akmal.alavi@rootcode.io>
2024-12-18 09:45:53 +0530

    fix merge conflict

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,9 @@ const nextConfig = {
       },
       {
         source: "/setup-organization",
-        destination: isEnterpriseMode ? "/enterprise/setup-organization" : "/community/setup-organization"
+        destination: isEnterpriseMode
+          ? "/enterprise/setup-organization"
+          : "/community/setup-organization"
       },
       {
         source: "/dashboard",
@@ -195,6 +197,10 @@ const nextConfig = {
       {
         source: "/verify-success",
         destination: "/enterprise/verify-success"
+      },
+      {
+        source: "/redirect",
+        destination: "/enterprise/redirect"
       }
     ];
   },
