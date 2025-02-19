67dfa1e08db0ae32136e519ca5999b60487b28c6
Shakya Karunathilake <shakya.karunathilake@rootcode.io>
2025-02-19 12:58:13 +0530

    feat: english file for settings

--- src/community/common/assets/languages/english/english.ts
+++ src/community/common/assets/languages/english/english.ts
@@ -4,6 +4,7 @@ import configurations from "~community/common/assets/languages/english/configura
 import leaveModule from "~community/common/assets/languages/english/leaveModule.json";
 import onboarding from "~community/common/assets/languages/english/onboarding.json";
 import peopleModule from "~community/common/assets/languages/english/peopleModule.json";
+import settings from "~enterprise/settings/assets/languages/english/settings.json";
 
 export const english = {
   ...common,
@@ -11,6 +12,7 @@ export const english = {
   configurations,
   peopleModule,
   leaveModule,
-  attendanceModule
+  attendanceModule,
+  settings
   // add other modules here
 };
