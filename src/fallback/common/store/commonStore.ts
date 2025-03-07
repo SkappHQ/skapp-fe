02adb829becf2d2bfbbd34cf5a18c2cac5395761
Akmal Alavi <akmal.alavi@rootcode.io>
2025-03-07 17:22:44 +0530

    fix: added fallback

--- src/fallback/common/store/commonStore.ts
+++ src/fallback/common/store/commonStore.ts
@@ -1,6 +1,15 @@
 interface CommonEnterpriseStore {
   setGlobalLoginMethod: (value: string) => void;
   globalLoginMethod: string;
+  ongoingQuickSetup: {
+    INVITE_EMPLOYEES: boolean;
+    DEFINE_TEAMS: boolean;
+    DEFINE_JOB_FAMILIES: boolean;
+    SETUP_HOLIDAYS: boolean;
+    SETUP_LEAVE_TYPES: boolean;
+  };
+  setQuickSetupModalType: (value: string) => void;
+  setStopAllOngoingQuickSetup: () => void;
 }
 
 export const useCommonEnterpriseStore = (
@@ -8,6 +17,15 @@ export const useCommonEnterpriseStore = (
 ): CommonEnterpriseStore => {
   return {
     setGlobalLoginMethod: () => {},
-    globalLoginMethod: ""
+    globalLoginMethod: "",
+    ongoingQuickSetup: {
+      INVITE_EMPLOYEES: false,
+      DEFINE_TEAMS: false,
+      DEFINE_JOB_FAMILIES: false,
+      SETUP_HOLIDAYS: false,
+      SETUP_LEAVE_TYPES: false
+    },
+    setQuickSetupModalType: () => {},
+    setStopAllOngoingQuickSetup: () => {}
   };
 };
