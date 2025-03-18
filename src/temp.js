a449f57513fa397711626a0d442a7e44a90b8444
Thinuwan <122858767+ThinuwanW@users.noreply.github.com>
2025-03-18 18:14:06 +0530

    Update temp.js

--- src/temp.js
+++ src/temp.js
@@ -1,6 +1,7 @@
 const STRIPE_SECRET_KEY = "sk_test_1234567890"; // ❌ Hardcoded Secret
 const JWT_SECRET = "mySuperSecretKey"; // ❌ Hardcoded JWT Secret
 
+// TODO: whatttttt
 export default async function handler(req, res) {
   res.json({ message: "Secrets are exposed!" });
 }
