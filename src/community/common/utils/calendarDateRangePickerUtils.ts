0683a2e8987f9ad718441c2d58650b111cf909e6
Shakya Karunathilake <shakya.karunathilake@rootcode.io>
2025-03-21 13:52:37 +0530

    fix: half day leave validation

--- src/community/common/utils/calendarDateRangePickerUtils.ts
+++ src/community/common/utils/calendarDateRangePickerUtils.ts
@@ -38,7 +38,7 @@ export const getMyLeaveRequestForDay = ({
   date
 }: GetMyLeaveRequestForDayProps) => {
   if (myLeaveRequests !== undefined) {
-    const myLeaveRequestForDay = myLeaveRequests?.find((leaveRequest) => {
+    const myLeaveRequestForDay = myLeaveRequests?.filter((leaveRequest) => {
       const startDate = DateTime.fromISO(leaveRequest.startDate);
       const endDate = DateTime.fromISO(leaveRequest.endDate);
 
@@ -71,16 +71,25 @@ export const handleDateChange = ({
   if (!date) return;
 
   if (isRangePicker) {
-    if (selectedDates.length === 2) {
-      setSelectedDates([date]);
-    } else if (selectedDates.length === 1) {
-      if (date > selectedDates[0]) {
-        setSelectedDates([selectedDates[0], date]);
-      } else {
-        setSelectedDates([date, selectedDates[0]]);
-      }
-    } else {
-      setSelectedDates([date]);
+    switch (selectedDates.length) {
+      case 0:
+        setSelectedDates([date]);
+        break;
+      case 1:
+        if (date !== selectedDates[0]) {
+          if (date > selectedDates[0]) {
+            setSelectedDates([selectedDates[0], date]);
+          } else {
+            setSelectedDates([date, selectedDates[0]]);
+          }
+        }
+        break;
+      case 2:
+        setSelectedDates([date]);
+        break;
+      default:
+        setSelectedDates([date]);
+        break;
     }
   } else {
     setSelectedDates([date]);
@@ -108,8 +117,12 @@ export const handleDateValidation = ({
 }: HandleDateValidationProps) => {
   if (!selectedDates) return;
 
+  let isApplyLeaveModalBtnDisabled = false;
+
   if (allowedDuration === LeaveDurationTypes.HALF_DAY) {
     if (selectedDates.length > 1) {
+      isApplyLeaveModalBtnDisabled = true;
+
       setToastMessage({
         key: MyRequestsToastMsgKeyEnums.ONLY_HALF_DAY_LEAVE_ALLOWED,
         open: true,
@@ -133,6 +146,8 @@ export const handleDateValidation = ({
       const holidayCount = holidays.length;
 
       if (holidayCount > 0) {
+        isApplyLeaveModalBtnDisabled = true;
+
         const hasMultipleHolidays = holidayCount > 1;
 
         const toastType = ToastType.WARN;
@@ -172,18 +187,20 @@ export const handleDateValidation = ({
     const leaveRequestCount = leaveRequests?.length ?? 0;
 
     if (leaveRequestCount > 0) {
-      if (selectedDates.length === 1) {
-        const isFullDayOrTwoHalfDayLeaves = leaveRequests.filter(
-          (leaveRequest) =>
-            leaveRequest.leaveState === LeaveStates.FULL_DAY ||
-            leaveRequests.length > 1
+      if (selectedDates.length === 1 || selectedDates[0] === selectedDates[1]) {
+        const leaveStates = leaveRequests.map(
+          (leaveRequest) => leaveRequest.leaveState
         );
 
-        setIsApplyLeaveModalBtnDisabled(
-          isFullDayOrTwoHalfDayLeaves.length !== 0
-        );
+        const hasFullDayLeave = leaveStates.includes(LeaveStates.FULL_DAY);
+        const hasHalfDayMorningAndEvening =
+          leaveStates.includes(LeaveStates.MORNING) &&
+          leaveStates.includes(LeaveStates.EVENING);
+
+        isApplyLeaveModalBtnDisabled =
+          hasFullDayLeave || hasHalfDayMorningAndEvening;
       } else {
-        setIsApplyLeaveModalBtnDisabled(leaveRequestCount > 0);
+        isApplyLeaveModalBtnDisabled = true;
       }
 
       const hasMultipleLeaves = leaveRequestCount > 1;
@@ -209,6 +226,8 @@ export const handleDateValidation = ({
         description
       });
     }
+
+    setIsApplyLeaveModalBtnDisabled(isApplyLeaveModalBtnDisabled);
   }
 };
 
@@ -267,7 +286,7 @@ export const getLeaveRequestsWithinDateRange = ({
   if (!myLeaveRequests) return [];
 
   const startDate = selectedDates[0];
-  const endDate = selectedDates[1] ?? startDate;
+  const endDate = selectedDates[1] ?? selectedDates[0];
 
   const leaveRequestsWithinRange = myLeaveRequests.filter(
     (leaveRequest: MyLeaveRequestPayloadType) => {
