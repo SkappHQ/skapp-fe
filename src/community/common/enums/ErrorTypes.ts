be4b3e4d856e450d77d7942e8dcb03296fe6f5fc
Shakya Karunathilake <108653862+shakyaatrootcode@users.noreply.github.com>
2025-03-28 17:42:30 +0530

    fix: people, leave module minor issues

--- src/community/common/enums/ErrorTypes.ts
+++ src/community/common/enums/ErrorTypes.ts
@@ -1,6 +1,7 @@
 export enum FileUploadErrorTypes {
-  INVALID_TEXT_OR_CSV_FILE_TYPE_ERROR = "File type must be text/csv,.csv",
+  INVALID_TEXT_OR_CSV_FILE_TYPE_ERROR = "File type must be one of text/csv, .csv",
   INVALID_IMAGE_OR_PDF_FILE_TYPE_ERROR = "File type must be image/jpeg,.jpg,.jpeg,image/png,application/pdf,.pdf",
+  INVALID_IMAGE_TYPE_ERROR = "File type must be one of image/*, .png, .jpeg",
   TOO_MANY_FILES_ERROR = "Too many files",
   MAX_FILE_SIZE_ERROR = "File is larger than 5000000 bytes"
 }
