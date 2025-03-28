011d12968ca0f5c78327c672cc9ee700437aefe7
hasalarootcode <hasala.abhilasha@rootcode.io>
2025-03-28 15:36:59 +0530

    refactor: Move route change confirmation logic into RouteChangeAreYouSureModal component

--- src/community/people/components/organisms/AccountSectionWrapper/AccountSectionWrapper.tsx
+++ src/community/people/components/organisms/AccountSectionWrapper/AccountSectionWrapper.tsx
@@ -1,13 +1,10 @@
-import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
+import { useEffect } from "react";
 
-import { useTranslator } from "~community/common/hooks/useTranslator";
 import { useGetEmployee } from "~community/people/api/PeopleApi";
 import useFormChangeDetector from "~community/people/hooks/useFormChangeDetector";
 import { usePeopleStore } from "~community/people/store/store";
 
 import DirectorySteppers from "../../molecules/DirectorySteppers/DirectorySteppers";
-import RouteChangeAreYouSureModal from "../../molecules/RouteChangeAreYouSureModal/RouteChangeAreYouSureModal";
 import UnsavedChangesModal from "../../molecules/UnsavedChangesModal/UnsavedChangesModal";
 import UserDetailsCentered from "../../molecules/UserDetailsCentered/UserDetailsCentered";
 import PeopleAccountSection from "../PeopleAccountSection/PeopleAccountSection";
@@ -54,55 +51,6 @@ const AccountSectionWrapper = ({ employeeId }: Props) => {
     setIsUnsavedChangesModalOpen
   ]);
 
-  const [isAreYouSureModalOpen, setIsAreYouSureModalOpen] = useState(false);
-  const allowRouteChangeRef = useRef<boolean>(false);
-  const targetRouteRef = useRef<string>("");
-
-  const router = useRouter();
-
-  const translateText = useTranslator(
-    "peopleModule",
-    "addResource",
-    "commonText"
-  );
-
-  const handleBeforeUnload = (e: BeforeUnloadEvent) => {
-    if (hasChanged) {
-      e.preventDefault();
-      return "";
-    }
-  };
-
-  const handleRouteChange = (url: string) => {
-    if (allowRouteChangeRef.current) return;
-    targetRouteRef.current = url;
-    if (hasChanged && !isAreYouSureModalOpen) {
-      setIsAreYouSureModalOpen(true);
-      router.events.emit("routeChangeError");
-      throw "routeChange aborted";
-    }
-  };
-
-  useEffect(() => {
-    router.events.on("routeChangeStart", handleRouteChange);
-    window.addEventListener("beforeunload", handleBeforeUnload);
-    return () => {
-      router.events.off("routeChangeStart", handleRouteChange);
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, [handleRouteChange, handleBeforeUnload]);
-
-  const handleModalResume = () => {
-    setIsAreYouSureModalOpen(false);
-  };
-
-  const handleModalLeave = async () => {
-    setIsAreYouSureModalOpen(false);
-    allowRouteChangeRef.current = true;
-    const targetRoute = targetRouteRef.current;
-    await router.push(targetRoute);
-  };
-
   return (
     <>
       {employee && (
@@ -125,12 +73,6 @@ const AccountSectionWrapper = ({ employeeId }: Props) => {
         onSave={() => {
           setIsUnsavedModalSaveButtonClicked(true);
         }}
-      />
-      <RouteChangeAreYouSureModal
-        isOpen={isAreYouSureModalOpen}
-        onResume={handleModalResume}
-        onLeave={handleModalLeave}
-        title={translateText(["areYouSureModalTitle"])}
       />
     </>
   );
