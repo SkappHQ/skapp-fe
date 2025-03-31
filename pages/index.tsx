e261eae9a33e1c8069c67585d24ee02acde1b5cd
hasalarootcode <hasala.abhilasha@rootcode.io>
2025-03-31 12:07:35 +0530

    hotfix: home page redirection issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,20 @@
+import { signOut } from "next-auth/react";
+import { useEffect } from "react";
+
 import { useRedirectHandler } from "~community/common/utils/hooks/useRedirectHandler";
 
 export default function Index() {
+  useEffect(() => {
+    if (!window.location.host.includes("app")) {
+      const tenant = window.location.host.split(".")[0];
+      if (tenant !== "app") {
+        signOut({
+          redirect: false
+        });
+      }
+    }
+  }, []);
+
   useRedirectHandler({ isSignInPage: false });
 
   return null;
