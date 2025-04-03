4623a01277074435666c5e8918edbdaa8ebc95c4
hasalarootcode <hasala.abhilasha@rootcode.io>
2025-04-03 13:33:09 +0530

    fix: update employee data filter to set active status correctly

--- src/community/people/utils/handleEmployeeDataFIlters.ts
+++ src/community/people/utils/handleEmployeeDataFIlters.ts
@@ -34,7 +34,9 @@ export const handleApplyFilterPrams = (
         : [EmploymentStatusTypes.ACTIVE]
     );
   } else {
-    setEmployeeDataParams(DataFilterEnums.ACCOUNT_STATUS, "");
+    setEmployeeDataParams(DataFilterEnums.ACCOUNT_STATUS, [
+      EmploymentStatusTypes.ACTIVE
+    ]);
   }
 
   if (
