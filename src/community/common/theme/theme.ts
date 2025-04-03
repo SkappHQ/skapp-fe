e2f443b75112834719e2163ccd4ba268d2494f59
thusala <thusala.piyarisi@rootcode.io>
2025-04-03 13:11:07 +0530

    fix: move to type

--- src/community/common/theme/theme.ts
+++ src/community/common/theme/theme.ts
@@ -1,19 +1,18 @@
 import { createTheme } from "@mui/material/styles";
 import * as React from "react";
 
-export interface OpacityPalette {
-  10: string;
-  20: string;
-  30: string;
-  40: string;
-  50: string;
-  60: string;
-  70: string;
-  80: string;
-  90: string;
-}
-
 declare module "@mui/material/styles" {
+  interface OpacityPalette {
+    10: string;
+    20: string;
+    30: string;
+    40: string;
+    50: string;
+    60: string;
+    70: string;
+    80: string;
+    90: string;
+  }
   interface Palette {
     opacity: OpacityPalette;
     notifyBadge: {
