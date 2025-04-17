9338a314182f544d3ee5f899499cf43dcaffb03a
Shakya Karunathilake <108653862+shakyaatrootcode@users.noreply.github.com>
2025-04-17 11:43:11 +0530

    fix: proper utils

--- src/community/common/enums/KeyboardEnums.ts
+++ src/community/common/enums/KeyboardEnums.ts
@@ -6,5 +6,9 @@ export enum KeyboardKeys {
   ARROW_UP = "ArrowUp",
   ARROW_DOWN = "ArrowDown",
   TAB = "Tab",
-  SPACE = " "
+  SPACE = " ",
+  HOME = "Home",
+  END = "End",
+  PAGE_UP = "PageUp",
+  PAGE_DOWN = "PageDown"
 }
