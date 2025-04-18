ff167bff46ba5d69d382601a3c0e88336d1c1d0c
Shakya Karunathilake <108653862+shakyaatrootcode@users.noreply.github.com>
2025-04-18 15:46:44 +0530

    fix: preprocessor

--- src/community/leave/utils/LeavePreprocessors.ts
+++ src/community/leave/utils/LeavePreprocessors.ts
@@ -113,11 +113,7 @@ export function leaveRequestDataPreProcessor(
     durationDays: data?.durationDays,
     dates: getStartEndDate(data.startDate, data.endDate),
 
-    days: getLeaveRequestsLeaveState(
-      data.startDate,
-      data.endDate,
-      data.leaveState
-    ),
+    days: `${data.durationDays} Days`,
     reason: data?.requestDesc ?? "",
     reviewerComment: data?.reviewerComment ?? "",
     reviewedDate: getStandardDate(data?.reviewedDate),
