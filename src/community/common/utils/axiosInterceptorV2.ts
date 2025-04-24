92a4272fa3bac3a87a8588ac7bd413b5ffa9025a
Akmal Alavi <akmal.alavi@rootcode.io>
2025-04-24 21:28:48 +0530

    fix: people directory api change

---
+++ src/community/common/utils/axiosInterceptorV2.ts
@@ -0,0 +1,43 @@
+import axios, { InternalAxiosRequestConfig } from "axios";
+import { getSession } from "next-auth/react";
+
+import { ApiVersions } from "../constants/configs";
+import { getApiUrl } from "./getConstants";
+
+const getSubDomain = (url: string, multipleValues: boolean = false) => {
+  const subdomain = multipleValues ? url.split(".") : url.split(".")[0];
+  return subdomain;
+};
+
+export const tenantID =
+  typeof window !== "undefined" ? getSubDomain(window.location.hostname) : "";
+
+const authFetchV2 = axios.create({
+  baseURL: getApiUrl() + ApiVersions.V2
+});
+
+//  request interceptor
+authFetchV2.interceptors.request.use(
+  async (config: InternalAxiosRequestConfig) => {
+    const session = await getSession();
+
+    if (
+      session?.user.accessToken &&
+      !config.url?.includes("/refresh-token") &&
+      !config.url?.includes("/app-setup-status")
+    ) {
+      config.headers.Authorization = `Bearer ${session?.user.accessToken}`;
+    }
+
+    const isEnterpriseMode = process.env.NEXT_PUBLIC_MODE === "enterprise";
+    if (isEnterpriseMode && tenantID) {
+      config.headers["X-Tenant-ID"] = tenantID;
+    }
+    return config;
+  },
+  async (error) => {
+    return await Promise.reject(error);
+  }
+);
+
+export default authFetchV2;
