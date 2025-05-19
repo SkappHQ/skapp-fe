d881be74401b4aa0462381f29da8c5f70aeec05e
HarinduMalinka <harindu.desilva@rootcode.io>
2025-05-19 15:21:49 +0530

    added number validation for phone number

--- backend/src/main/java/com/skapp/community/common/util/Validation.java
+++ backend/src/main/java/com/skapp/community/common/util/Validation.java
@@ -43,6 +43,7 @@ public class Validation {
 
 	public static final String PHONE_NUMBER_PATTERN = "^[0-9]{7,15}$";
 
+
 	public static final String VALID_IDENTIFICATION_NUMBER_REGEXP = "^[a-zA-Z0-9\\-_]*$";
 
 	public static final String VALID_NIN_NUMBER_REGEXP = "^[a-zA-Z0-9]*$";
