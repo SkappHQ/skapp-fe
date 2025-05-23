77c11c7ce71d3f5644f73d69618887b6540b6965
Akmal Alavi <akmal.alavi@rootcode.io>
2025-05-23 22:25:11 +0530

    fix: use default theme for sign flow

--- frontend/src/community/common/components/templates/ContentLayout/ContentLayout.tsx
+++ frontend/src/community/common/components/templates/ContentLayout/ContentLayout.tsx
@@ -183,9 +183,13 @@ const ContentLayout = ({
 
   const { data: organizationDetails } = useGetOrganization();
 
-  const updatedTheme = themeSelector(
-    organizationDetails?.results?.[0]?.themeColor || ThemeTypes.BLUE_THEME
-  );
+  const useDefaultTheme = asPath.split("?")[0] === ROUTES.SIGN.SIGN;
+
+  const themeColor = useDefaultTheme
+    ? ThemeTypes.BLUE_THEME
+    : organizationDetails?.results?.[0]?.themeColor || ThemeTypes.BLUE_THEME;
+
+  const updatedTheme = themeSelector(themeColor);
 
   theme.palette = updatedTheme.palette;
 
